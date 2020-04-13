fd283b2789f6c302f36b9f2b1b20e1ca29988d7b
Robert Garrison <robert.garrison@unityfisolutions.com>
2020-04-13 16:00:31 -0400

    Updating async methods to use ShouldBePrepared from ISqlQuery that determines if a query should be prepared for .net standard 2.1 and above.  Updating to ADO.Net.Client.Implementation 1.0.5

--- src/ADO.Net.Client/DbAsynchronousClient.cs
+++ src/ADO.Net.Client/DbAsynchronousClient.cs
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 #endregion
-#region Using Declarations
+#region Using Statements
 using ADO.Net.Client.Core;
 using System.Collections.Generic;
 using System.Data;
