d0fc53f27e4eaf833250f8f8899c949068c239ed
Robert Garrison <robert.garrison@unityfisolutions.com>
2020-04-23 20:00:19 -0400

    Annotation updates

--- src/ADO.Net.Client.Annotations/ANSIStringFixedLength.cs
+++ src/ADO.Net.Client.Annotations/ANSIStringFixedLength.cs
@@ -29,11 +29,11 @@ using System.Data;
 namespace ADO.Net.Client.Annotations
 {
     /// <summary>
-    /// Signifies if the data type is <see cref="DbType.AnsiString"/> of varying length
+    /// Signifies if the data type is <see cref="DbType.AnsiString"/> of a fixed length
     /// </summary>
     /// <seealso cref="Attribute"/>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-    public class ANSIString : Attribute
+    public class ANSIStringFixedLength : Attribute
     {
         #region Fields/Properties
         #endregion
