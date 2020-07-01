fd3c465fe80693b4f8ff4da2837654bb8abf0323
Robert Garrison <robert.garrison@unityfisolutions.com>
2020-07-01 18:11:27 -0400

    removing unecessary using statement

--- src/ADO.Net.Client.Core/IDbObjectFactory.cs
+++ src/ADO.Net.Client.Core/IDbObjectFactory.cs
@@ -25,7 +25,6 @@ SOFTWARE.*/
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 #endregion
